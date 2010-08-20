2a991d41c5fa2fa028aeceaad12f022f632e70d5
John Hicks <johnhicks@gmail.com>
2010-08-20 16:13:51 -0700

    Non implemented types now throw runtime exceptions

--- src/org/andwellness/xml/datagenerator/InJSONGeneratorType.java
+++ src/org/andwellness/xml/datagenerator/InJSONGeneratorType.java
@@ -7,9 +7,7 @@ import org.json.JSONArray;
 public class InJSONGeneratorType implements JSONGeneratorType {
 
     public JSONArray translateDataPointsToJsonArray(List<DataPoint> dataPointList) {
-        JSONArray translatedJSON = new JSONArray();
-        
-        return translatedJSON;
+        throw new UnsupportedOperationException("Input JSON generator not yet implemented!");
     }
 
 }
