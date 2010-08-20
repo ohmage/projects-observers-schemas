6be63bf21da9ad9f6a3ebd7652325648e75aba19
John Hicks <johnhicks@gmail.com>
2010-08-19 17:14:04 -0700

    Changed DataPoint to be a purely getter/setter for AndWellness prompts. This way we can use a JSON translator to translate to JSON in different ways (i.e., phone to server JSON or server to viz JSON)

--- src/org/andwellness/xml/datagenerator/DataPoint.java
+++ src/org/andwellness/xml/datagenerator/DataPoint.java
@@ -1,59 +1,89 @@
 package org.andwellness.xml.datagenerator;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class DataPoint {
-    // Possible data types
-    public static enum TYPE {
+    // List of data fields in a data point, add more if necessary for extended data points
+    private String id;     // The id of the data point
+    private String value;  
+    
+    // Metadata
+    private String unit;
+    private String datetime;
+    private String tz;
+    private double lat;
+    private double lon;
+    private DataPoint.DisplayType displayType; 
+    
+    
+    // Possible display types
+    public static enum DisplayType {
         category, measurement, event, counter 
     }
     
-    // Map to hold the data point in a format easily JSONified
-    private Map<String, Object> jsonMap = new HashMap<String, Object>();
-    
     // Nothing to do here!
     public DataPoint() {}
-    
-    // Grab the created json map for whatever
-    public Map<String, Object> getJson() {
-        return jsonMap;
-    }
-    
+        
     // Large set of putters to create the json map
-    public void putLabel(String label) {
-        jsonMap.put("label", label);
+    public void setId(String _id) {
+        id = _id;
     }
     
-    public void putValue(String value) {
-        jsonMap.put("value", value);
+    public String getId() {
+        return id;
+    }
+    
+    public void setValue(String _value) {
+        value = _value;
     }
 
     public String getValue() {
-        return (String) jsonMap.get("value");
+        return value;
     }
     
-    public void putUnit(String unit) {
-        jsonMap.put("unit", unit);
+    public void setUnit(String _unit) {
+        unit = _unit;
     }
     
-    public void putDatetime(String datetime) {
-        jsonMap.put("datetime", datetime);
+    public String getUnit() {
+        return unit;
     }
     
-    public void putTz(String tz) {
-        jsonMap.put("tz", tz);
+    public void setDatetime(String _datetime) {
+        datetime = _datetime;
     }
     
-    public void putLat(double lat) {
-        jsonMap.put("lat", String.valueOf(lat));
+    public String getDatetime() {
+        return datetime;
     }
     
-    public void putLon(double lon) {
-        jsonMap.put("lon", String.valueOf(lon));
+    public void setTz(String _tz) {
+        tz = _tz;
     }
     
-    public void putType(TYPE type) {
-        jsonMap.put("type", type.toString());
+    public String getTz() {
+        return tz;
+    }
+    
+    public void setLat(double _lat) {
+        lat = _lat;
+    }
+    
+    public double getLat() {
+        return lat;
+    }
+    
+    public void setLon(double _lon) {
+        lon = _lon;
+    }
+    
+    public double getLon() {
+        return lon;
+    }
+    
+    public void setDisplayType(DisplayType _displayType) {
+        displayType = _displayType;
+    }
+    
+    public DisplayType getDisplayType() {
+        return displayType;
     }
 }
