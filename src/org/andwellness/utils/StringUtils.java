fef820635a0a289c9cb02474a270728e2b430b22
John Jenkins <jojenki@cens.ucla.edu>
2011-07-08 08:35:40 -0700

    Added better URN checking to the XML validation.

--- src/org/andwellness/utils/StringUtils.java
+++ src/org/andwellness/utils/StringUtils.java
@@ -1,11 +1,14 @@
 package org.andwellness.utils;
 
+import java.util.regex.Pattern;
+
 /**
  * A utility class for analyzing String values.
  * 
  * @author John Jenkins
  */
 public class StringUtils {
+	private static Pattern _urnPattern = Pattern.compile("[a-z0-9_]+");
 	
 	/**
 	 * Hidden default constructor as this is a helper class but not one that
@@ -28,5 +31,41 @@ public class StringUtils {
 		
 		return null == string || "".equals(string.trim()); 
 		
+	}
+	
+	/**
+	 * Validates that a URN is valid.
+	 * 
+	 * @return Returns true if the provided value is a valid URN; otherwise, it
+	 * 		   returns false.
+	 */
+	public static boolean isValidUrn(String value) {
+		if(isEmptyOrWhitespaceOnly(value)) {
+			return false;
+		}
+		
+		String s = value.toLowerCase();
+		
+		if(! s.startsWith("urn:")) {
+			return false;
+		}
+		
+		if(s.length() < 7) { // disallow anything shorter than urn:a:a 
+			return false;
+		}
+		
+		String[] a = s.split(":");
+		
+		if(a.length < 3) { // require at least three colon-delimited sections
+			return false;
+		}
+		
+		for(int i = 1; i < a.length; i++) { // each section after the initial urn must match _urnPattern
+			if(! _urnPattern.matcher(a[i]).matches()) {
+				return false;
+			}
+		}
+		
+		return true;
 	}
 }
